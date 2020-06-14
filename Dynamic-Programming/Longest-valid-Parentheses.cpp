e91d22b4a67ee3ee8d41ffe4c876ff6c70a4d6d1
Lakshay wadhwa <lakshaywadhwa.cse20@jecrc.ac.in>
2020-06-14 14:09:20 +0530

    Create Longest-valid-Parentheses.cpp

---
+++ Dynamic-Programming/Longest-valid-Parentheses.cpp
@@ -0,0 +1,31 @@
+https://www.interviewbit.com/problems/longest-valid-parentheses/
+
+int Solution::longestValidParentheses(string s) {
+    stack<int>st;
+	int ans=0,cur=0;
+	int i=0;
+	while(i<s.length())
+	{
+		if(s[i]=='(')
+		{
+			st.push(cur);
+			cur=0;
+		}
+		else
+		{
+			if(st.empty())
+			{
+				cur=0;
+			}
+			else
+			{
+				cur=cur+st.top()+2;
+				st.pop();
+				ans=max(cur,ans);
+			}
+		}
+		i++;
+
+	}
+	return ans;
+}
